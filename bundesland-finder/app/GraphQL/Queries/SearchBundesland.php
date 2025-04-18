d6f1ce84dc7c054c10d8f4b37c9345b0b4d8e003
tinwan.ng <tinwan.ng@govmind.tech>
2025-04-18 04:10:54 +0200

    feat: add schema and resolver

---
+++ bundesland-finder/app/GraphQL/Queries/SearchBundesland.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Bundesland;
+use App\Models\Bezirk;
+use App\Models\Kreis;
+
+class SearchBundesland
+{
+    public function __invoke($_, array $args)
+    {
+        $name = strtolower($args['name']);
+
+        // Match Bezirk
+        $bezirkBundeslandIds = Bezirk::whereRaw('LOWER(name) LIKE ?', ["%{$name}%"])
+            ->pluck('bundesland_id');
+
+        // Match Kreis
+        $kreisBundeslandIds = Kreis::whereRaw('LOWER(name) LIKE ?', ["%{$name}%"])
+            ->with('bezirk')
+            ->get()
+            ->pluck('bezirk.bundesland_id');
+
+        $allBundeslandIds = $bezirkBundeslandIds->merge($kreisBundeslandIds)->unique();
+
+        return Bundesland::whereIn('id', $allBundeslandIds)->get();
+    }
+}
+
